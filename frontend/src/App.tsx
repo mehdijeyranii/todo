5a5fb6304a7237f8335489017a7a39b632f59281
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-15 18:11:40 +0330

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Todo App!</h1>
+    </div>
+  );
+};
+
+export default App;
