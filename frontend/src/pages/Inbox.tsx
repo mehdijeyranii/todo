a579244b4a60aec3f34a6f37a68dfea099d79073
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-17 11:26:29 +0330

    Test

--- frontend/src/pages/Inbox.tsx
+++ frontend/src/pages/Inbox.tsx
@@ -1,7 +1,12 @@
 const Inbox = () => {
   return (
     <div>
-      <h1>Inbox page</h1>
+      <h1>Mehdi Jeyrani</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo suscipit
+        ad doloribus eum inventore est totam perferendis omnis numquam sunt quia
+        consequuntur repellat sequi sint officia blanditiis, optio odio unde.
+      </p>
     </div>
   );
 };
